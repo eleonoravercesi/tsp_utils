b9726ab121ceaeb53a7f8edd771fef1fe67ec25b
eleonoravercesi <eleonora.vercesi@libero.it>
2025-10-13 14:50:15 +0200

    Update __init__.py

--- tsp_utils/__init__.py
+++ tsp_utils/__init__.py
@@ -1,2 +1,2 @@
-from .stsp import solve_tsp, solve_tsp_fixed_edge, run_concorde, dual_sep, solve_sep, write_tsplib
+from .stsp import solve_tsp, solve_tsp_fixed_edge, run_concorde, dual_sep, solve_sep, write_tsplib, from_edge_list_to_tour
 from .tsp import run_LKH
